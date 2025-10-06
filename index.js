44e0e93437181a4bae3cc97d2ffdcb311d65399d
mohammadomarccc <101573548+mohammadomarccc@users.noreply.github.com>
2025-10-06 18:26:36 +0300

    Commit init files

---
+++ index.js
@@ -0,0 +1,32 @@
+let home = document.getElementById("home-count") ;
+let guest = document.getElementById("guest-count") ;
+
+
+function add1_home(){
+    home.textContent = Number(home.textContent) + 1
+}
+
+function add2_home(){
+    home.textContent = Number(home.textContent) + 2
+    
+}
+
+function add3_home(){
+    home.textContent = Number(home.textContent) + 3
+    
+}
+
+function add1_guest(){
+    guest.textContent = Number(guest.textContent) + 1
+
+}
+
+function add2_guest(){
+    guest.textContent = Number(guest.textContent) + 2
+    
+}
+
+function add3_guest(){
+    guest.textContent = Number(guest.textContent) + 3
+    
+}
